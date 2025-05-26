fe0474c016b314a6d87ac1554616c439f6f47511
Vinicius Peres <vinniemalafaia@hotmail.com>
2025-05-26 03:24:27 -0400

    created on order status updated emitter

--- server/src/domain/realtime/application/subscribers/on-order-status-updated.spec.ts
+++ server/src/domain/realtime/application/subscribers/on-order-status-updated.spec.ts
@@ -1,62 +1,71 @@
-import { OnOrderCreated } from './on-order-created'
 import { waitFor } from 'test/utils/wait-for'
 import { InMemoryOnlineUsersRepository } from 'test/repositories/in-memory-online-users.repository'
 import { FakeSocketProvider } from 'test/providers/fake-socket.provider'
 import { makeAttendant } from 'test/factories/make-attendant'
 import { makeOrder } from 'test/factories/make-order'
-import { InMemoryAttendantsRepository } from 'test/repositories/in-memory-attendants.repository'
 import { InMemoryUsersRepository } from 'test/repositories/in-memory-users.repository'
 import { InMemoryOrdersRepository } from 'test/repositories/in-memory-orders.repository'
 import { InMemoryOrderItemsRepository } from 'test/repositories/in-memory-orderItems.repository'
 import { MockInstance } from 'vitest'
 import { makeOnlineUser } from 'test/factories/make-online-user'
 import { makeCook } from 'test/factories/make-cook'
+import { OnOrderStatusUpdated } from './on-order-status-updated'
 
 let onlineUsersRepository: InMemoryOnlineUsersRepository
 let socketProvider: FakeSocketProvider
-let attendantsRepository: InMemoryAttendantsRepository
 let usersRepository: InMemoryUsersRepository
 let orderItemsRepository: InMemoryOrderItemsRepository
 let ordersRepository: InMemoryOrdersRepository
 
-let emitNewOrderEmitToSpy: MockInstance<(typeof socketProvider)['emitTo']>
+let emitOrderStatusUpdatedEmitToSpy: MockInstance<
+  (typeof socketProvider)['emitTo']
+>
 
-describe('On order created', () => {
+describe('On order status updated', () => {
   beforeEach(() => {
     onlineUsersRepository = new InMemoryOnlineUsersRepository()
     socketProvider = new FakeSocketProvider()
 
-    attendantsRepository = new InMemoryAttendantsRepository()
     usersRepository = new InMemoryUsersRepository()
     orderItemsRepository = new InMemoryOrderItemsRepository()
     ordersRepository = new InMemoryOrdersRepository(orderItemsRepository)
 
-    emitNewOrderEmitToSpy = vi.spyOn(socketProvider, 'emitTo')
+    emitOrderStatusUpdatedEmitToSpy = vi.spyOn(socketProvider, 'emitTo')
 
-    new OnOrderCreated(onlineUsersRepository, socketProvider)
+    new OnOrderStatusUpdated(onlineUsersRepository, socketProvider)
   })
 
-  it('should emit a socket event when an order is created', async () => {
+  it('should emit a socket event when an order status is updated', async () => {
     const attendant = makeAttendant()
+    const offlineCook = makeCook()
     const cook = makeCook()
     const order = makeOrder({
       attendantId: attendant.id
     })
 
-    await attendantsRepository.create(attendant)
+    await usersRepository.create(attendant)
+    await usersRepository.create(offlineCook)
     await usersRepository.create(cook)
 
-    const onlineUser = makeOnlineUser({
+    const onlineUser01 = makeOnlineUser({
+      userId: attendant.id
+    })
+    const onlineUser02 = makeOnlineUser({
       userId: cook.id
     })
 
-    await onlineUsersRepository.add(onlineUser)
+    await onlineUsersRepository.add(onlineUser01)
+    await onlineUsersRepository.add(onlineUser02)
     await ordersRepository.create(order)
 
+    order.status = 'processing'
+
+    await ordersRepository.save(order)
+
     await waitFor(() => {
-      expect(emitNewOrderEmitToSpy).toHaveBeenCalled()
-      expect(onlineUsersRepository.onlineUsers.size).toBe(1)
-      expect(socketProvider.emitted).toHaveLength(1)
+      expect(emitOrderStatusUpdatedEmitToSpy).toHaveBeenCalled()
+      expect(onlineUsersRepository.onlineUsers.size).toBe(2)
+      expect(socketProvider.emitted).toHaveLength(2)
     })
   })
 })
