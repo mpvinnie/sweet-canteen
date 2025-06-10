bbf545eca8d55ec06322932ce19266d79be7c6cb
Vinicius Peres <vinniemalafaia@hotmail.com>
2025-06-10 10:41:49 -0400

    config new eslint in web

---
+++ web/eslint.config.js
@@ -0,0 +1,42 @@
+import css from '@eslint/css'
+import js from '@eslint/js'
+import json from '@eslint/json'
+import pluginEslintPrettier from 'eslint-plugin-prettier/recommended'
+import pluginReact from 'eslint-plugin-react'
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    plugins: { js },
+    extends: ['js/recommended']
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    languageOptions: { globals: globals.browser }
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    files: ['**/*.json'],
+    plugins: { json },
+    language: 'json/json',
+    extends: ['json/recommended']
+  },
+  {
+    files: ['**/*.css'],
+    plugins: { css },
+    language: 'css/css',
+    extends: ['css/recommended']
+  },
+  pluginEslintPrettier,
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'prettier/prettier': 'error'
+    }
+  }
+])
