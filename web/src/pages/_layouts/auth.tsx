3e1b5d95d80be63b5e36c16ced3e3876eda6b793
Vinicius Peres <vinniemalafaia@hotmail.com>
2025-06-12 07:04:11 -0400

    feat: login page layout

--- web/src/pages/_layouts/auth.tsx
+++ web/src/pages/_layouts/auth.tsx
@@ -1,11 +1,21 @@
+import { Pizza } from 'lucide-react'
 import { Outlet } from 'react-router'
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Authentication</h1>
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="border-foreground/5 bg-muted text-muted-foreground flex h-full flex-col justify-between border-r p-10">
+        <div className="text-foreground flex items-center gap-3 text-lg">
+          <Pizza className="h-5 w-5" />
+          <span className="font-semibold">sweet.canteen</span>
+        </div>
+        <footer className="text-sm">
+          Painel de gerenciamento &copy; sweet.canteen -{' '}
+          {new Date().getFullYear()}
+        </footer>
+      </div>
 
-      <div>
+      <div className="flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
--- web/src/pages/auth/sign-in.tsx
+++ web/src/pages/auth/sign-in.tsx
@@ -1,7 +1,36 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 import { useDocumentTitle } from '@/hooks/useDocumentTitle'
 
 export function SignIn() {
   useDocumentTitle('Sign In | sweet.canteen')
 
-  return <h1>Sign In</h1>
+  return (
+    <div className="p-8">
+      <div className="flex w-[350px] flex-col justify-center gap-6">
+        <div className="flex flex-col gap-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Acessar painel
+          </h1>
+          <p className="text-muted-foreground text-sm">
+            Acompanhe e gerencie os pedidos pelo painel!
+          </p>
+        </div>
+
+        <form className="space-y-4">
+          <div className="space-y-2">
+            <Label htmlFor="email">Seu usuário</Label>
+            <Input id="email" />
+          </div>
+          <div className="space-y-2">
+            <Label htmlFor="password">Sua senha</Label>
+            <Input id="password" type="password" />
+          </div>
+
+          <Button className="w-full">Acessar painel</Button>
+        </form>
+      </div>
+    </div>
+  )
 }
